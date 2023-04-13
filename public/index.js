a1aff2429e8a7bb68d48d3e7de72349c516ef21a
achrefbz <achrefbouzgarrou@yahoo.com>
2023-04-13 18:47:40 +0100

    second

--- public/index.js
+++ public/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/App';
 import reportWebVitals from '../src/reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -30,4 +31,5 @@ reportWebVitals();
       }, false)
     })
   })()
+  
   
